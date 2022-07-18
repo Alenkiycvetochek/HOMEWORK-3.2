730691241b8c1749ae2ee3553e0d9bde1009441a
Alina Alinushka <alinaalinushka@MacBook-Pro-Alina.local>
2022-07-19 00:06:15 +0300

    created task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// / Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void PaliandromMeth (string n, string s)
+{   
+    char[] nReverse = n.ToCharArray();
+    Array.Reverse(nReverse);
+    s = new string(nReverse);
+    
+    if (n == s)
+    Console.WriteLine (n + "This number is paliandrom");
+    else
+    Console.WriteLine (n + "This number is not poliandrom");
+}
+
+Console.WriteLine("Input five digit number:");
+string num = Convert.ToString(Console.ReadLine());
+
+PaliandromMeth(num, num);
