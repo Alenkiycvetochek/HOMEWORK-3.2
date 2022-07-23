ebefbd0243fbb0423862885e4e37078dcb183003
Alina Alinushka <alinaalinushka@MacBook-Pro-Alina.local>
2022-07-23 09:48:03 +0300

    created Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Kub (int a)
+{
+    for (int i = 1 ;i<=a; i++)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
+
+Console.Write("Input number: ");
+int kub = Convert.ToInt32(Console.ReadLine());
+
+Kub (kub);
